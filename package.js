74726ac552c56ae7dfc2b550fd6be05ebb890d85
David Burles <dburles@gmail.com>
2013-11-26 14:55:50 +1100

    initial

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  summary: 'Factories for Meteor'
+});
+
+Package.on_use(function(api) {
+  api.add_files('lib/factory.js', ['client', 'server']);
+  api.export('Factory', ['client', 'server']);
+  api.export('Factories', ['client', 'server']);
+});
