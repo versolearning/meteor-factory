aca6272b01b30d4f623cb3ca51efe93017f77a6d
David Burles <dburles@gmail.com>
2015-12-17 23:19:57 +1100

    es2015

--- factory.js
+++ factory.js
@@ -1,114 +1,105 @@
 /* global LocalCollection */
 /* global Factory:true */
 
-var factories = {};
+const factories = {};
 
-Factory = function(name, collection, attributes) {
-  this.name = name;
-  this.collection = collection;
-  this.attributes = attributes;
-  this.afterHooks = [];
-  this.sequence = 0;
+Factory = class Factory {
+  constructor(name, collection, attributes) {
+    this.name = name;
+    this.collection = collection;
+    this.attributes = attributes;
+    this.afterHooks = [];
+    this.sequence = 0;
+  }
+
+  after(fn) {
+    this.afterHooks.push(fn);
+    return this;
+  }
 };
 
-Factory.define = function(name, collection, attributes) {
+Factory.define = (name, collection, attributes) => {
   factories[name] = new Factory(name, collection, attributes);
   return factories[name];
 };
 
-Factory.get = function(name) {
-  var factory = factories[name];
-  if (!factory) {
+Factory.get = name => {
+  const factory = factories[name];
+  if (! factory) {
     throw new Error("Factory: There is no factory named " + name);
   }
   return factory;
 };
 
-Factory.build = function(name, attributes, options) {
-  var factory = Factory.get(name);
-  var base = {};
-  attributes = attributes || {};
-  options = options || {};
+Factory.build = (name, attributes = {}, options = {}) => {
+  const factory = Factory.get(name);
+  const result = {};
 
   // "raw" attributes without functions evaluated, or dotted properties resolved
-  attributes = _.extend(base, factory.attributes, attributes);
-
-  var result = {};
+  extendedAttributes = _.extend({}, factory.attributes, attributes);
 
   // either create a new factory and return its _id
   // or return a 'fake' _id (since we're not inserting anything)
-  var makeRelation = function(relName) {
+  const makeRelation = relName => {
     if (options.insert) {
       return Factory.create(relName)._id;
     }
-
     // fake an id on build
     return Random.id();
   };
 
   factory.sequence += 1;
 
-  var walk = function(record, object) {
-    _.each(object, function(value, key) {
+  const walk = (record, object) => {
+    _.each(object, (value, key) => {
+      let newValue = value;
       // is this a Factory instance?
       if (value instanceof Factory) {
-        value = makeRelation(value.name);
-      }
-      else if (_.isFunction(value)) {
-        var fnRes = value.call(result, {
-          sequence: function(fn) {
-            return fn(factory.sequence);
-          }
+        newValue = makeRelation(value.name);
+      } else if (_.isFunction(value)) {
+        const fnRes = value.call(result, {
+          sequence: fn => fn(factory.sequence)
         });
         // does executing this function return a Factory instance?
-        value = (fnRes instanceof Factory) ? makeRelation(fnRes.name) : fnRes;
+        newValue = (fnRes instanceof Factory) ? makeRelation(fnRes.name) : fnRes;
       // if an object literal is passed in, traverse deeper into it
-      }
-      else if (Object.prototype.toString.call(value) === "[object Object]") {
+      } else if (Object.prototype.toString.call(value) === '[object Object]') {
         record[key] = record[key] || {};
         return walk(record[key], value);
       }
 
-      var modifier = {$set: {}};
-      if (key !== "_id") {
-        modifier.$set[key] = value;
+      const modifier = {$set: {}};
+
+      if (key !== '_id') {
+        modifier.$set[key] = newValue;
       }
 
       LocalCollection._modify(record, modifier);
     });
   };
 
-  walk(result, attributes);
+  walk(result, extendedAttributes);
 
-  result._id = attributes._id || Random.id();
+  result._id = extendedAttributes._id || Random.id();
   return result;
 };
 
-Factory._create = function(name, doc) {
-  var collection = Factory.get(name).collection;
-  var insertId = collection.insert(doc);
-  var record = collection.findOne(insertId);
+Factory._create = (name, doc) => {
+  const collection = Factory.get(name).collection;
+  const insertId = collection.insert(doc);
+  const record = collection.findOne(insertId);
   return record;
 };
 
-Factory.create = function(name, attributes) {
-  attributes = attributes || {};
-  var doc = Factory.build(name, attributes, {insert: true});
-  var record = Factory._create(name, doc);
+Factory.create = (name, attributes = {}) => {
+  const doc = Factory.build(name, attributes, {insert: true});
+  const record = Factory._create(name, doc);
 
-  _.each(Factory.get(name).afterHooks, function(cb) {
-    cb(record);
-  });
+  Factory.get(name).afterHooks.forEach(cb => cb(record));
 
   return record;
 };
 
-Factory.extend = function(name, attributes) {
-  attributes = attributes || {};
+Factory.extend = (name, attributes = {}) => {
   return _.extend(_.clone(Factory.get(name).attributes), attributes);
-};
-
-Factory.prototype.after = function(fn) {
-  this.afterHooks.push(fn);
-  return this;
 };
