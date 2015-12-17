acdc89cf25ed6c11c1e85046d84148c12994ae2f
David Burles <dburles@gmail.com>
2015-12-17 22:51:33 +1100

    update package.js

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'dburles:factory',
   summary: 'Factories for Meteor',
-  version: '0.3.10',
+  version: '0.3.11',
   git: 'https://github.com/versolearning/meteor-factory.git'
 });
 
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   api.use(['minimongo', 'underscore', 'ejson', 'random']);
-  api.add_files('lib/factory.js', ['client', 'server']);
-  api.export('Factory', ['client', 'server']);
+  api.add_files('factory.js');
+  api.export('Factory');
 });
 
 Package.on_test(function(api) {
-  api.use(['tinytest', 'dburles:factory']);
-  api.add_files('lib/factory_tests.js', 'server');
+  api.use(['tinytest', 'dburles:factory', 'underscore']);
+  api.add_files('factory_tests.js', 'server');
 });
