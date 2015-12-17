5bd3667b03f7d32d69507453608b833e862a26cc
David Burles <dburles@gmail.com>
2015-12-17 22:46:34 +1100

    single quotes

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: "dburles:factory",
-  summary: "Factories for Meteor",
-  version: "0.3.10",
-  git: "https://github.com/versolearning/meteor-factory.git"
+  name: 'dburles:factory',
+  summary: 'Factories for Meteor',
+  version: '0.3.10',
+  git: 'https://github.com/versolearning/meteor-factory.git'
 });
 
 Package.on_use(function(api) {
-  api.versionsFrom("METEOR@0.9.0");
-  api.use(["minimongo", "underscore", "ejson", "random"]);
-  api.add_files("lib/factory.js", ["client", "server"]);
-  api.export("Factory", ["client", "server"]);
+  api.versionsFrom('METEOR@0.9.0');
+  api.use(['minimongo', 'underscore', 'ejson', 'random']);
+  api.add_files('lib/factory.js', ['client', 'server']);
+  api.export('Factory', ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-  api.use(["tinytest", "dburles:factory"]);
-  api.add_files("lib/factory_tests.js", "server");
+  api.use(['tinytest', 'dburles:factory']);
+  api.add_files('lib/factory_tests.js', 'server');
 });
