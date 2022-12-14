addf368728fd931465a76f9f3682debbe19d4afd
David Burles <dburles@gmail.com>
2022-12-14 11:34:35 +1100

    1.3.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "dburles:factory",
   summary: "Factories for Meteor",
-  version: "1.2.0",
+  version: "1.3.0",
   git: "https://github.com/versolearning/meteor-factory.git",
 });
 
